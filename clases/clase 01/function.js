998de2fba3ffd7838bb0a24cbead1b38be456acf
Mayra Zapari <kraka.nord.8@gmail.com>
2024-01-10 19:09:22 -0800

    primeras clases de javascript

---
+++ clases/clase 01/function.js
@@ -0,0 +1,11 @@
+const quieroSumar = function sumar2Numeros(a,b){
+  if(typeof a === 'number' && typeof b === 'number'){
+    let rersultado = a + b
+
+    return resultado
+  } else{
+
+  }
+}
+
+console.log(quieroSumar(3,'Hola'))
